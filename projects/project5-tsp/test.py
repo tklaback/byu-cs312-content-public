34d2ac8065a561725019309f28a0b40cfa1d54b1
TY <klabacka.ty@gmail.com>
2023-11-22 17:33:12 -0700

    added functionality to skip row and col in get_bound

--- projects/project5-tsp/test.py
+++ projects/project5-tsp/test.py
@@ -4,21 +4,25 @@
 bssf = float('inf')
 lower_bound = 0
 
-def get_bound(arr: list) -> tuple:
+def get_bound(arr: list, avoid_row: int = None, avoid_col: int = None) -> tuple:
     cur_bound = 0
-        
-    def increment_and_return(arr: list):
-        nonlocal cur_bound
-        min_val = min(arr)
-        cur_bound += min_val
-
-        return [x - min_val for x in arr]
-
     
     #returns new reduced matrix and bound corresponding to it
-    new_list = [increment_and_return(arr[row]) for row in range(len(arr))] # list(map(lambda x: x - min(arr[row]) if x != float('inf') else float('inf'), arr[row]))
+    new_list = []
 
+    for row in range(len(arr)):
+        if row == avoid_row:
+            continue
+        tmp_lyst = []
+        min_val = min(arr[row])
+        cur_bound += min_val
+        for col in range(len(arr[0])):
+            tmp_lyst.append(arr[row][col] - min_val)
+        new_list.append(tmp_lyst)
+    
     for row in range(len(new_list)):
+        if row == avoid_col:
+            continue
         min_col_num = min([new_list[col][row] for col in range(len(new_list[0]))])
         cur_bound += min_col_num
         for col in range(len(new_list[0])):
@@ -26,8 +30,23 @@ def get_bound(arr: list) -> tuple:
     
     return (new_list, cur_bound)
 
+def expand(arr: list, state: int = None):
+    for row in range(len(arr)):
+        for col in range(len(arr[0])):
+            get_bound(arr, row, col)
+
+
+
 def process(arr: list):
-    pass
+    get_bound(arr)
+
+    q = [arr]
+
+    while len(q) != 0:
+        cur_arr = arr.pop(0)
+        if get_bound(cur_arr)[1] < bssf:
+            pass
+
 
 arr = \
 [
