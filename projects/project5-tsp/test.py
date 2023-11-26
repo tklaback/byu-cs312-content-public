6b0bb81fcab66adf580dddef382cf4f90d3df245
TY <klabacka.ty@gmail.com>
2023-11-26 13:25:30 -0700

    made some progress

--- projects/project5-tsp/test.py
+++ projects/project5-tsp/test.py
@@ -15,6 +15,9 @@ def get_bound(arr: list, avoid_row: int = None, avoid_col: int = None) -> tuple:
 
     for row in range(len(arr)):
         if row == avoid_row:
+            for col in range(len(arr[0])):
+                tmp_lyst.append(float('inf'))
+            new_list.append(tmp_lyst)
             continue
         tmp_lyst = []
         min_val = min(arr[row])
@@ -25,6 +28,8 @@ def get_bound(arr: list, avoid_row: int = None, avoid_col: int = None) -> tuple:
     
     for row in range(len(new_list)):
         if row == avoid_col:
+            for col in range(len(new_list[0])):
+                new_list[col][row] = float('inf')
             continue
         min_col_num = min([new_list[col][row] for col in range(len(new_list[0]))])
         cur_bound += min_col_num
@@ -34,9 +39,16 @@ def get_bound(arr: list, avoid_row: int = None, avoid_col: int = None) -> tuple:
     return (new_list, cur_bound)
 
 def expand(arr: list, row_num: int, prev_bound: int, remaining_path_cost: int):
+    expansion = []
     for col in range(len(arr[row_num])):
         if arr[row_num][col] != float('inf'):
             new_matrix, bound = get_bound(arr, row_num, col)
+            if (prev_bound + remaining_path_cost + bound) < bssf:
+                expansion.append([(row_num, col), (prev_bound + remaining_path_cost + bound)])
+    
+    return expansion
+
+# TODO: How do I test the current subproblem to see if it is a complete tour?
 
 
 
@@ -54,10 +66,11 @@ def process(arr: list):
 
 arr = \
 [
-    [float('inf'), 5, 4, 3], 
-    [3, float('inf'), 8, 2], 
-    [5, 3, float('inf'), 9], 
-    [6, 4, 3, float('inf')]
+    [float('inf'), 9           , float('inf'), 8           , float('inf')], 
+    [float('inf'), float('inf'), 4           , float('inf'), 2           ], 
+    [float('inf'), 3           , float('inf'), 4           , float('inf')], 
+    [float('inf'), 6           , 7           , float('inf'), 12          ]
+    [1           , float('inf'), float('inf'), 10          , float('inf')]
 ]
 
 
