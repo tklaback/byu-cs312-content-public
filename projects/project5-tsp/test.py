29eff4b8400d0318fe82a1a35de603cefec2e2e9
TY <klabacka.ty@gmail.com>
2023-11-28 12:42:43 -0700

    realized that I don't need to keep track of which rows and cols I previously blocked

--- projects/project5-tsp/test.py
+++ projects/project5-tsp/test.py
@@ -4,23 +4,20 @@
 bssf = float('inf')
 lower_bound = 0
 
-def get_bound(arr: list, avoid_row: list = None, avoid_col: list = None) -> tuple:
+def get_bound(arr: list, avoid_row: int = None, avoid_col: int = None) -> tuple:
     cur_bound = 0
 
     
     
     #returns new reduced matrix and bound corresponding to it
     if avoid_row != None and avoid_col != None:
-        if len(avoid_col) != len(avoid_row):
-            raise Exception("AVOID ROWS AND COLUMNS DO NOT MATCH")
-        for itr in range(len(avoid_row)):
-            arr[avoid_col[itr]][avoid_row[itr]] = float('inf')
+        arr[avoid_col][avoid_row] = float('inf')
     
     new_list = []
 
     for row in range(len(arr)):
         tmp_lyst = []
-        if avoid_row and row in avoid_row:
+        if row == avoid_row:
             for col in range(len(arr[0])):
                 tmp_lyst.append(float('inf'))
             new_list.append(tmp_lyst)
@@ -32,7 +29,7 @@ def get_bound(arr: list, avoid_row: list = None, avoid_col: list = None) -> tupl
         new_list.append(tmp_lyst)
     
     for row in range(len(new_list)):
-        if avoid_col and row in avoid_col:
+        if row == avoid_col:
             for col in range(len(new_list[0])):
                 new_list[col][row] = float('inf')
             continue
@@ -43,14 +40,14 @@ def get_bound(arr: list, avoid_row: list = None, avoid_col: list = None) -> tupl
     
     return (new_list, cur_bound)
 
-def expand(arr: list, prev_bound: int, cur_lyst: list, avoid_rows: list, avoid_cols: list):
+def expand(arr: list, prev_bound: int, cur_lyst: list):
     expansion = []
     row_num = cur_lyst[-1]
     for col in range(len(arr[row_num])):
         if arr[row_num][col] != float('inf'):
-            new_matrix, bound = get_bound(arr, avoid_rows + [row_num], avoid_cols + [col])
+            new_matrix, bound = get_bound(arr, row_num, col)
             if (prev_bound + arr[row_num][col] + bound) < bssf:
-                expansion.append([new_matrix, (prev_bound + arr[row_num][col] + bound), cur_lyst[:] + [col], avoid_rows + [row_num], avoid_cols + [col]])
+                expansion.append([new_matrix, (prev_bound + arr[row_num][col] + bound), cur_lyst[:] + [col]])
     
     return expansion
 
@@ -71,12 +68,12 @@ def process(arr: list):
     lyst = [0]
     new_list, new_bound = get_bound(arr)
 
-    q = [(new_list, new_bound, lyst, [], [])]
+    q = [(new_list, new_bound, lyst)]
 
     while len(q) != 0:
         cur_element = q.pop(0)
         if cur_element[1] < bssf:
-            arr_of_matrices = expand(cur_element[0], cur_element[1], cur_element[2], cur_element[3], cur_element[4])
+            arr_of_matrices = expand(cur_element[0], cur_element[1], cur_element[2])
             for itm in arr_of_matrices:
                 if test(itm[2], len(arr)):
                     bssf = itm[1]
